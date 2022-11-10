968d94ad8cb4d54cb507ec0c855f5ecbef597f5b
DavidSilveira80 <davidpscontato@gmail.com>
2022-11-10 20:32:53 -0300

    Implementado primeiro teste.

--- niveis/src/estruturado/HelloWorld.java
+++ niveis/src/estruturado/HelloWorld.java
@@ -1,9 +1,12 @@
 package estruturado;
 
 public class HelloWorld {
+	public static String hello() {
+	    return "Hello World!";
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println(hello());
 
 	}
 
--- niveis/src/testes/TesteHello.java
+++ niveis/src/testes/TesteHello.java
@@ -1,14 +1,14 @@
 package testes;
-
-import static org.junit.jupiter.api.Assertions.*;
-
+import estruturado.HelloWorld;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TesteHello {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void hello_world() {
+		Assertions.assertEquals("Hello World!", HelloWorld.hello());
+		
 	}
 
 }
