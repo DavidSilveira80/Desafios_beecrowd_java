40ed8053da35ec2f2fe645e92a4aa809bf365288
DavidSilveira80 <davidpscontato@gmail.com>
2022-11-13 18:09:40 -0300

    Enunciado conforme beecrowd.

--- iniciante/src/test/java/TestHelloWorld.java
+++ iniciante/src/test/java/TestHelloWorld.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Assertions;
 public class TestHelloWorld {
     @Test
     void hello_world(){
-        Assertions.assertEquals("Hello World Java!!!", HelloWorld.hello());
+        Assertions.assertEquals("Hello World!", HelloWorld.hello());
     }
 
 }
