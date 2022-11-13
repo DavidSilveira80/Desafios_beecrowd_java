d4890e815435fd6364af338a8a031c01141317e5
DavidSilveira80 <davidpscontato@gmail.com>
2022-11-13 19:49:20 -0300

    Solução do desafio extremamente básico com testes.

---
+++ iniciante/src/main/java/ExtremamenteBasico.java
@@ -0,0 +1,48 @@
+/*
+Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B
+atribuindo o seu resultado na variável X. Imprima X conforme exemplo apresentado abaixo.
+Não apresente mensagem alguma além daquilo que está sendo especificado e
+não esqueça de imprimir o fim de linha após o resultado, caso contrário,
+você receberá "Presentation Error".
+
+Entrada
+A entrada contém 2 valores inteiros.
+
+Saída
+Imprima a mensagem "X = " (letra X maiúscula) seguido pelo valor da variável X e pelo
+final de linha. Cuide para que tenha um espaço antes e depois do sinal de igualdade,
+conforme o exemplo abaixo.
+
+Exemplos de Entrada	            Exemplos de Saída
+10                               X = 19
+9
+
+-10                              X = -6
+4
+
+
+15                               X = 8
+-7
+
+ */
+
+import java.util.Scanner;
+
+public class ExtremamenteBasico {
+    public static int soma_A_B(int A, int B ){
+        return A + B;
+    }
+
+    public static String soma_saida(int A, int B){
+        return String.format("X = %d", soma_A_B(A, B));
+    }
+
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        int A = entrada.nextInt();
+        int B = entrada.nextInt();
+
+        System.out.println(soma_saida(A, B));
+
+    }
+}
