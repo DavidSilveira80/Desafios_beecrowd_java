9c8d977986bb8f55617b8c8e9fea862a77111543
DavidSilveira80 <davidpscontato@gmail.com>
2022-11-13 20:56:38 -0300

    Desafio área do círculo resolvido e com testes.

---
+++ iniciante/src/test/java/TestAreaCirculo.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class TestAreaCirculo {
+    AreaCirculo area = new AreaCirculo();
+    @Test
+    void area_calc_circulo(){
+
+        Assertions.assertEquals(12.56636, area.area_calculo(2.00));
+
+    }
+    @Test
+    void area_circulo_saida(){
+        Assertions.assertEquals("A=12.5664", area.saida_area_circulo(2.00));
+    }
+
+}
