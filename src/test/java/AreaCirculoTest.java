19e3dfadb18dc7d32ff08e0b265bacddb5db3d34
DavidSilveira80 <davidpscontato@gmail.com>
2023-02-16 18:52:43 -0300

    junit5

--- src/test/java/AreaCirculoTest.java
+++ src/test/java/AreaCirculoTest.java
@@ -2,17 +2,19 @@ import iniciante.AreaCirculo;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
-public class TestAreaCirculo {
+
+
+public class AreaCirculoTest {
     AreaCirculo area = new AreaCirculo();
+
     @Test
-    void test_area_calc_circulo(){
+    void areaDoCirculoTest(){
 
         Assertions.assertEquals(12.56636, area.area_calculo(2.00));
 
     }
     @Test
-    void area_circulo_saida(){
+    void area_circulo_saidaTest(){
         Assertions.assertEquals("A=12.5664", area.saida_area_circulo(2.00));
     }
-
 }
